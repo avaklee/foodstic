5a6b2bfa995b774a003d87f13c23505c429109b7
Ava Lee <avaklee@gmail.com>
2017-06-21 18:54:24 +0900

    introducing devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :drivers
+  devise_for :stores
+  devise_for :users
    root to: "stores#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
